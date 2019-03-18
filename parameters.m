849428a53c999747f66bd7434c34373a4fdbce1c
jtsauls <saulsopolis@gmail.com>
2019-03-18 12:39:41 -0700

    exp specific

--- parameters.m
+++ parameters.m
@@ -4,17 +4,17 @@
 % Intensity_weighting, Cycle_Picker, stat_cell_param, cellcycle_mat2csv
 px_to_mu = 0.11;
 
-data_dir = '/Volumes/JunLabSSD_04/bsub_unitcell/20190311_BS45_32/analysis/cell_data/';
+data_dir = '/Volumes/JunLabSSD_04/bsub_unitcell/20190227_BS45_26/fl/analysis/cell_data/';
 
-cell_foci_file_path = [data_dir 'b32_mothercells_1120_foci.mat'];
+cell_foci_file_path = [data_dir 'b26_fl_mothercells_690_foci.mat'];
 
 
 % Used by Cycle_Picker and plot_channel
-channel_start = 1;
+channel_start = 55;
 
-xlim_minimum = 160;
-xlim_maximum = 660;
+xlim_minimum = 40;
+xlim_maximum = 220;
 ylim_maximum = 6;
 
 % foci above this value will have two foci. 
-IW_thr = 6000;
+IW_thr = 3000;
